6a4274cee15eb8e26e3bc9b95c0cacc816130bda
Mauricio Gonzalez <0236968@up.edu.mx>
2023-02-15 23:31:24 -0600

    Prueba

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Componentes/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../index.css'
+
+function Header(){
+    return (
+        <div className = "Header">
+            <div>
+                <div>
+                    <h1>Mauricio<br></br>González</h1>
+                    <hr></hr>
+                    <p>Instagram | Linkedin</p>
+                    <p>Behance | Dribble</p>
+                </div>
+                <div>
+                     <h1>Hola</h1>
+                </div>
+                <div>
+                    <h1>Hola</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Header; 
